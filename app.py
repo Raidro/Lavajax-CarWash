a3224c1b33aca1de315963ef7b91827b807d68c9
Mateus Sátiro <mateus_sat@hotmail.com>
2019-05-16 22:25:39 -0300

    adicionado o dado do gps

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ POSTGRES = {
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 
-postgres = CREATE DATABASE Lavajax;
+postgres = CREATE
+DATABASE
+Lavajax;
 
 db.init_app(app)
 manager = Manager(app)
@@ -33,10 +35,20 @@ class BaseModel(db):
 __abstract__ = True
 
 
-class YourModel(BaseModel):
+class Users(BaseModel):
     # """model for one of your table"""
     __tablename__ = 'usuario'
+
     # define your model
+
+
+class GPS(BaseModel, db.Model):
+    """Model for the stations table"""
+    __tablename__ = 'gps'
+
+    id = db.Column(db.Integer, primary_key=True)
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
 
 
 @app.route("/")
